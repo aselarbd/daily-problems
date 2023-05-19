9451cf904d8291c179dd9776cf6e6782900f59ee
Asela Dasanayaka <aselarbd@gmail.com>
2023-05-19 23:22:52 +0530

    #65-problem-2 19-05-2023

--- Linked List /234. Palindrome Linked List.py
+++ Linked List /234. Palindrome Linked List.py
@@ -49,4 +49,30 @@ class Solution:
 
         return True
 
+
 # Solution 2
+class Solution2:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast, slow = head, head
+
+        # find the middle (slow)
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # reverse second half
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        # check palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
